3aac795682faa7580fdd227cb0da3adaa5416452
LuciferHuang <784863142@qq.com>
2026-05-02 01:34:26 +0800

    chore(xhr): update author name and optimize context access

--- browser_plugins/xhr/src/index.ts
+++ browser_plugins/xhr/src/index.ts
@@ -24,18 +24,18 @@ function XHRPlugin(options: RequestPluginOptionType = {}): BasePluginType {
     name: 'XHRPlugin',
     monitor(notify: (data: HttpCollectDataType) => void) {
       const { initUrl, uploadUrl } = this.getContext();
-      const client = this;
+      const getTime = this.getTime.bind(this);
       const ignore = [...ignoreUrls, uploadUrl, initUrl].map((url) => getUrlPath(url));
       replaceOld(originalXhrProto, 'open', (originalOpen: voidFun): voidFun => {
         return function (this: XMLHttp, ...args: any[]): void {
-          this.httpCollect = {
-            req: {
-              m: args[0] ? args[0].toUpperCase() : args[0],
-              url: args[1]
-            },
-            res: {},
-            t: client.getTime()
-          };
+            this.httpCollect = {
+              req: {
+                m: args[0] ? args[0].toUpperCase() : args[0],
+                url: args[1]
+              },
+              res: {},
+              t: getTime()
+            };
           originalOpen.apply(this, args);
         };
       });
@@ -48,7 +48,7 @@ function XHRPlugin(options: RequestPluginOptionType = {}): BasePluginType {
             if (isBlock) return;
             const { responseType, response, status } = this;
             req.data = args[0];
-            const eTime = client.getTime();
+            const eTime = getTime();
             if (reportResponds && ['', 'json', 'text'].indexOf(responseType) !== -1) {
               this.httpCollect.res.dat = typeof response === 'object' ? JSON.stringify(response) : response;
             }
